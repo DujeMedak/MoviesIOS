4dc426ac80bf2eb00da7e138f45af8049eb22a59
Duje Medak <duje.medak@fer.hr>
2018-05-14 11:59:18 +0200

    ImageDetail zooming implemented

--- Movies/ImageDetailViewController.swift
+++ Movies/ImageDetailViewController.swift
@@ -8,12 +8,22 @@
 
 import UIKit
 
-class ImageDetailViewController: UIViewController {
+class ImageDetailViewController: UIViewController, UIScrollViewDelegate{
 
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var movieImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        scrollView.minimumZoomScale = 1.0
+        scrollView.maximumZoomScale = 5.0
+        
+        scrollView.delegate = self
+    
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +31,15 @@ class ImageDetailViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return movieImage
     }
-    */
+
 
 }
+/*
+extension ImageDetailViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return movieImage
+    }
+}*/
