138551a5ba6bc50dd374f34d556d2bf37020dc83
Duje Medak <duje.medak@fer.hr>
2018-05-15 13:36:45 +0200

    constrains in MovieDetails added

--- Movies/MovieDetailsViewController.swift
+++ Movies/MovieDetailsViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Kingfisher
+import PureLayout
 
 
 class MovieDetailsViewController: UIViewController , EditViewControllerDelegate{
@@ -23,6 +24,10 @@ class MovieDetailsViewController: UIViewController , EditViewControllerDelegate{
     @IBOutlet weak var movieImage: UIImageView!
     
     
+    @IBOutlet weak var yearTxt: UILabel!
+    @IBOutlet weak var genreTxt: UILabel!
+    @IBOutlet weak var directorTxt: UILabel!
+    
     convenience init() {
      self.init(model: nil)
      }
@@ -36,29 +41,65 @@ class MovieDetailsViewController: UIViewController , EditViewControllerDelegate{
      super.init(coder: aDecoder)
      }
     
+    func setConstrains(){
+        //imageView Constrains done in interface builder
+        movieTitle.autoPinEdge(.top, to: ALEdge.bottom, of: movieImage)
+        movieTitle.autoAlignAxis(toSuperviewAxis: .vertical)
+        
+        
+        yearTxt.autoPinEdge(toSuperviewEdge: .left, withInset: 16)
+        genreTxt.autoPinEdge(toSuperviewEdge: .left, withInset: 16)
+        directorTxt.autoPinEdge(toSuperviewEdge: .left, withInset: 16)
+        movieDescription.autoPinEdge(toSuperviewEdge: .left, withInset: 16)
+        
+        yearTxt.autoPinEdge(.top, to: ALEdge.bottom, of: movieTitle)
+        genreTxt.autoPinEdge(.top, to: ALEdge.bottom, of: yearTxt)
+        directorTxt.autoPinEdge(.top, to: ALEdge.bottom, of: genreTxt)
+        movieDescription.autoPinEdge(.top, to: ALEdge.bottom, of: directorTxt)
+        
+        year.autoPinEdge(.top, to: ALEdge.bottom, of: movieTitle)
+        genre.autoPinEdge(.top, to: ALEdge.bottom, of: year)
+        director.autoPinEdge(.top, to: ALEdge.bottom, of: genre)
+        movieDescription.autoPinEdge(.top, to: ALEdge.bottom, of: director)
+        
+        
+        year.autoAlignAxis(.baseline, toSameAxisOf: yearTxt)
+        genre.autoAlignAxis(.baseline, toSameAxisOf: genreTxt)
+        director.autoAlignAxis(.baseline, toSameAxisOf: directorTxt)
+        
+        year.autoAlignAxis(toSuperviewAxis: .vertical)
+        genre.autoAlignAxis(toSuperviewAxis: .vertical)
+        director.autoAlignAxis(toSuperviewAxis: .vertical)
+
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "edit", style: .done, target: self, action: #selector(onEditButtonTap))
         
+         setConstrains()
+        
         
         movieImage.isUserInteractionEnabled = true
         movieImage.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(MovieDetailsViewController.onImageViewTap(_:))))
         
-         year.text = String(model!.year)
-         genre.text = model!.genre.rawValue
-         director.text = model!.director.name + " " + model!.director.surname
-         movieDescription.text = model!.description
-         movieTitle.text = model!.title
-        
-         let url = URL(string: model!.imageURI)
-        
+        let url = URL(string: model!.imageURI)
         
         movieImage.kf.setImage(with: url, completionHandler: {
             (image, error, cacheType, imageUrl) in
             //is this part executed if the image is cached??
             self.img = image
-        })
+         })
+        
+        movieTitle.text = model!.title
+        
+        year.text = String(model!.year)
+        genre.text = model!.genre.rawValue
+        director.text = model!.director.name + " " + model!.director.surname
+        movieDescription.text = model!.description
+        
+        
         
         
     }
