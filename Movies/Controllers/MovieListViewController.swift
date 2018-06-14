b6ad562f6442dcae5df301cbc87e33c77dcbcf0d
Duje Medak <duje.medak@fer.hr>
2018-06-14 17:33:34 +0200

    animation of table view added

--- Movies/Controllers/MovieListViewController.swift
+++ Movies/Controllers/MovieListViewController.swift
@@ -49,6 +49,23 @@ class MovieListViewController: UIViewController{
         spinnerView = MovieListViewController.displaySpinner(onView: self.view)
         viewModel.fetchMovies()
     }
+    
+    func animateTable() {
+        let cells = tableView.visibleCells
+        let tableViewHeight = tableView.bounds.size.height
+        
+        for cell in cells {
+            cell.transform = CGAffineTransform(translationX: 0, y: tableViewHeight)
+        }
+        
+        var delayCounter = 0
+        for cell in cells {
+            UIView.animate(withDuration: 1.75, delay: Double(delayCounter) * 0.05, usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: .curveEaseInOut, animations: {
+                cell.transform = CGAffineTransform.identity
+            }, completion: nil)
+            delayCounter += 1
+        }
+    }
 }
 
 extension MovieListViewController: UITableViewDelegate {
@@ -104,6 +121,7 @@ extension MovieListViewController: MovieListDelegate{
         }
         else{
             tableView.reloadData()
+            animateTable()
             tableHeaderView.setTitle(title: "Results of searching " + viewModel.search)
             tableView.tableFooterView = nil
         }
