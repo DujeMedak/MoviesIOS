2b7e7d10a171fdf43901f59dd94d4015ffa27b8c
Duje Medak <duje.medak@fer.hr>
2018-06-14 17:10:24 +0200

    animation added in SeachView

--- Movies/Controllers/SearchMovieViewController.swift
+++ Movies/Controllers/SearchMovieViewController.swift
@@ -16,11 +16,39 @@ class SearchMovieViewController: UIViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.searchText.delegate = self as UITextFieldDelegate
+        setInitialStatesOfViews()
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         self.navigationController?.isNavigationBarHidden = true
         searchText.text = ""
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        animateEntry()
+    }
+    
+    func animateEntry(){
+        let duration = 0.4
+        UIView.animate(withDuration: duration, delay: 0, options: .curveEaseOut, animations: {
+            self.searchText.alpha = 1
+            self.searchText.layer.borderColor = UIColor.black.withAlphaComponent(1).cgColor
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+        UIView.animate(withDuration: duration, delay: 0, options: .curveEaseOut, animations: {
+            self.button_search.alpha = 1
+            self.button_search.layer.borderColor = UIColor.black.withAlphaComponent(1).cgColor
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+    }
+    
+    func setInitialStatesOfViews(){
+        searchText.alpha = 0.1
+        searchText.layer.borderColor = UIColor.black.withAlphaComponent(0.1).cgColor
+        button_search.alpha = 0.1
+        button_search.layer.borderColor = UIColor.black.withAlphaComponent(0.1).cgColor
     }
     
     override func viewWillDisappear(_ animated: Bool) {
